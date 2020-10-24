beac7547801199d23c67f97e0e0f87c05af5c1b5
Benjamin <benjamin.guy.thomas@gmail.com>
2020-10-24 11:33:06 +0200

    Add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './credentials/components/login/login.component';
-import { SignupComponent } from './credentials/components/signup/signup.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './credentials/components/login/login.component';
+import {SignupComponent} from './credentials/components/signup/signup.component';
 import {AppRoutingModule} from './routing/app-routing.module';
-import { CredentialsFormComponent } from './credentials/components/credentials-form/credentials-form.component';
-import { RemindersListComponent } from './components/reminders/reminders-list/reminders-list.component';
+import {CredentialsFormComponent} from './credentials/components/credentials-form/credentials-form.component';
+import {RemindersListComponent} from './components/reminders/reminders-list/reminders-list.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
-import { ReminderFormComponent } from './components/reminders/reminder-form/reminder-form.component';
-import { ReminderAddComponent } from './components/reminders/reminder-add/reminder-add.component';
-import { ReminderEditComponent } from './components/reminders/reminder-edit/reminder-edit.component';
+import {ReminderFormComponent} from './components/reminders/reminder-form/reminder-form.component';
+import {ReminderAddComponent} from './components/reminders/reminder-add/reminder-add.component';
+import {ReminderEditComponent} from './components/reminders/reminder-edit/reminder-edit.component';
 import {AuthInterceptor} from './auth.interceptor';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {ExpandMenuDirective} from './expand-menu.directive';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import {AuthInterceptor} from './auth.interceptor';
     RemindersListComponent,
     ReminderFormComponent,
     ReminderAddComponent,
-    ReminderEditComponent
+    ReminderEditComponent,
+    NavBarComponent,
+    ExpandMenuDirective,
   ],
   imports: [
     BrowserModule,
