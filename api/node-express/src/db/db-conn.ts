ccaa7171e958546187a4abe67e0f9eec838980c0
Benjamin <benjamin.guy.thomas@gmail.com>
2020-12-23 16:17:16 +0100

    Fix timestamp issue

--- api/node-express/src/db/db-conn.ts
+++ api/node-express/src/db/db-conn.ts
@@ -1,5 +1,14 @@
 import {mustEnv} from '../utils';
-import {Pool} from 'pg';
+import {Pool, types} from 'pg';
 
 const connectionString = mustEnv('DATABASE_URL');
 export const pool = new Pool({connectionString});
+
+/*
+ Override:
+   https://github.com/brianc/node-pg-types/blob/c4f8c120/lib/textParsers.js#L46
+
+ And cancel the added 'Z' char which messes with Angular's date pipe.
+*/
+const timeStampOID = types.builtins.TIMESTAMP;
+types.setTypeParser(timeStampOID, (val) => val);
