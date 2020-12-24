55e55ddde3a8bdaeb601fb07420fc6b15c47f710
Benjamin <benjamin.guy.thomas@gmail.com>
2020-12-24 07:52:48 +0100

    Basic golang/gin setup

---
+++ api/golang-gin/README.md
@@ -0,0 +1,9 @@
+## Setup
+
+NOTE: I do not use `GOPATH` anymore, so must be provided if downloading a binary.
+
+`GOPATH=~/code/go/ go get -v github.com/ivpusic/rerun`
+
+Then launch the dev server:
+
+`rerun`
---
+++ api/golang-gin/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	r := gin.Default()
+	log.Println("Initializing gin3...")
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	log.Fatalln(r.Run()) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
