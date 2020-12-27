880296f16df5d712f0aaaddb6a0c50d0a53506e8
Benjamin <benjamin.guy.thomas@gmail.com>
2020-12-27 09:57:54 +0100

    Add hello world spring boot app

---
+++ api/java-springboot/src/test/java/invalid/bt/remindersapi/RemindersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package invalid.bt.remindersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
